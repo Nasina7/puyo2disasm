320742cec1ab80a8e459f919addc2136596cb895
Nasina7 <38535607+Nasina7@users.noreply.github.com>
2025-05-11 21:12:55 -0400

    Initial Commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+echo "===The assembler will produce 4 'out of range' errors"
+echo "===This is intentional and is required to match the original rom"
+echo
+./tools/vasmm68k_psi-x -Fbin -m68000 -no-opt -o out/puyo2built.bin puyo2.asm
+echo
+echo "===Rom has been outputted to out/puyo2built.bin"
